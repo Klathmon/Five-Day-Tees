14a3e168134b59c43d138ef7d67fc7c0b126c992
Greg <gregbenner1@gmail.com>
2013-09-09 19:49:14 -0400

    Updated Vault page to use new objects

--- Includes/Controllers/Vault.php
+++ Includes/Controllers/Vault.php
@@ -10,10 +10,22 @@ $layout = new Layout($config, 'Vault.tpl', 'Vault', 'Page_Vault');
 if (!$layout->isPageCached()) {
     //If the page is cached, then skip all of this because it's not needed or used
     $settings    = new Settings($database, $config);
-    $itemsMapper = new \Mapper\Item($database, $config);
+    $itemsFactory = new \Factory\Item($database, $settings);
 
-    $layout->assign('settings', $settings);
-    $layout->assign('items', $itemsMapper->getVault(true));
+    $items = $itemsFactory->getVault();
+
+    foreach($items as $item){
+        $category = $settings->getItemCategory($item);
+        $itemsDisplay[] = [
+            'URLName' => $item->getURLName(),
+            'name' => $item->getDesign()->getName(),
+            'description' => $item->getFirstArticle()->getDescription(),
+            'price' => $settings->getItemCurrentPrice($item->getFirstArticle()->getBaseRetail(), $category)->getNiceFormat(),
+            'designImageURL' => $item->getDesign()->getFormattedDesignImage(150, 150, 'jpg')
+        ];
+    }
+
+    $layout->assign('items', $itemsDisplay);
 }
 
 $layout->output();
