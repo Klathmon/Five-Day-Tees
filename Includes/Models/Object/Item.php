9533ef8928739072da68aed726bc421ec307581e
Greg <gregbenner1@gmail.com>
2013-09-09 19:22:41 -0400

    Added getFirstArticle()

--- Includes/Models/Object/Item.php
+++ Includes/Models/Object/Item.php
@@ -64,6 +64,17 @@ class Item implements ObjectInterface
         return $this->products[$ID];
     }
 
+    /**
+     * @return Article
+     */
+    public function getFirstArticle()
+    {
+        return reset($this->articles);
+    }
+
+    /**
+     * @return string
+     */
     public function getURL()
     {
         return urlencode(str_replace(' ', '_', $this->getDesign()->getName()));
