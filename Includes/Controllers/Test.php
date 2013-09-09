af7be97385bab343fa54b0c7f4d83f5eec4652e8
Greg <gregbenner1@gmail.com>
2013-09-09 18:34:12 -0400

    Added page queries

--- Includes/Controllers/Test.php
+++ Includes/Controllers/Test.php
@@ -6,42 +6,10 @@
 
 echo 'Test Page!';
 
+$settings = new Settings($database, $config);
 
-$spreadshirt = new SpreadshirtItems($database, $config);
+$itemFactory = new \Factory\Item($database, $settings);
 
-$spreadshirt->getNewItems(200);
+$items = $itemFactory->getVault();
 
-/*
-$statement = $database->prepare(
-    <<<SQL
-    SELECT TestArticles.*, TestDesigns.*, TestProducts.*
-  FROM TestArticles
-    LEFT JOIN TestDesigns ON (TestArticles.DesignID = TestDesigns.ID)
-    LEFT JOIN TestProducts ON (TestArticles.ProductID = TestProducts.ID)
-SQL
-);
-
-$statement->execute();
-
-$array = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-$designs = [];
-
-foreach ($array as $row) {
-    $designs[$row['DesignID']]['Name']                                          = $row['Name'];
-    $designs[$row['DesignID']]['DisplayDate']                                   = DateTime::createFromFormat('Y-m-d', $row['DisplayDate']);
-    $designs[$row['DesignID']]['SalesLimit']                                    = $row['SalesLimit'];
-    $designs[$row['DesignID']]['DesignImageURL']                                = $row['DesignImageURL'];
-    $designs[$row['DesignID']]['Votes']                                         = $row['Votes'];
-    $designs[$row['DesignID']]['Articles'][$row['ID']]['ProductID']             = $row['ProductID'];
-    $designs[$row['DesignID']]['Articles'][$row['ID']]['LastUpdated']           = DateTime::createFromFormat('Y-m-d H:i:s', $row['LastUpdated']);
-    $designs[$row['DesignID']]['Articles'][$row['ID']]['Description']           = $row['Description'];
-    $designs[$row['DesignID']]['Articles'][$row['ID']]['ArticleImageURL']       = $row['ArticleImageURL'];
-    $designs[$row['DesignID']]['Articles'][$row['ID']]['NumberSold']            = $row['NumberSold'];
-    $designs[$row['DesignID']]['Products'][$row['ProductID']]['Cost']           = $row['Cost'];
-    $designs[$row['DesignID']]['Products'][$row['ProductID']]['Type']           = $row['Type'];
-    $designs[$row['DesignID']]['Products'][$row['ProductID']]['SizesAvailable'] = $row['SizesAvailable'];
-}
-
-\Debug::dump($array, $designs);
-*/
+var_dump($items);
--- Includes/Models/Factory/Item.php
+++ Includes/Models/Factory/Item.php
@@ -6,11 +6,11 @@
 
 namespace Factory;
 
-use ConfigParser;
 use PDO;
 use \Factory\Design;
 use \Factory\Article;
 use \Factory\Product;
+use Settings;
 
 /**
  * Class Item
@@ -20,8 +20,8 @@ use \Factory\Product;
  */
 class Item extends FactoryBase implements FactoryInterface
 {
-    /** @var ConfigParser */
-    protected $config;
+    /** @var Settings */
+    protected $settings;
     /** @var Design */
     protected $designFactory;
     /** @var Article */
@@ -36,14 +36,24 @@ SELECT
     Article.ID AS articleID, Article.lastUpdated, Article.description, Article.articleImageURL, Article.numberSold, Article.baseRetail,
     Product.ID AS productID, Product.cost, Product.type, Product.sizesAvailable
   FROM Design
-    RIGHT JOIN Article ON (designID = Article.designID)
-    LEFT JOIN Product ON (productID = Product.ID)
+    LEFT JOIN Article ON (Design.ID = Article.designID)
+    LEFT JOIN Product ON (Article.productID = Product.ID)
 SQL;
 
-    public function __construct(PDO $database, ConfigParser $config)
+    private $itemCountJoin
+        = <<<SQL
+LEFT JOIN (
+    SELECT designID, SUM(numberSold) AS totalSold 
+        FROM Article 
+        GROUP BY designID
+    ) AS itemsSold ON (Design.ID = itemsSold.designID)
+SQL;
+
+
+    public function __construct(PDO $database, Settings $settings)
     {
         parent::__construct($database);
-        $this->config        = $config;
+        $this->settings       = $settings;
         $this->designFactory  = new Design($database);
         $this->articleFactory = new Article($database);
         $this->productFactory = new Product($database);
@@ -56,7 +66,7 @@ SQL;
      */
     public function getByID($designID)
     {
-        $sql = $this->queryStart . ' WHERE Design.ID=:ID';
+        $sql = $this->queryStart . 'WHERE Design.ID=:ID';
 
         $statement = $this->database->prepare($sql);
         $statement->bindValue(':ID', $designID, PDO::PARAM_INT);
@@ -64,7 +74,7 @@ SQL;
 
         $array = $this->parseResults($statement->fetchAll(PDO::FETCH_ASSOC));
 
-        return $this->convertArrayToObject($array);
+        return $this->convertArrayToObject(reset($array));
 
     }
 
@@ -75,7 +85,7 @@ SQL;
      */
     public function getByName($name)
     {
-        $sql = $this->queryStart . ' WHERE Design.name = :name';
+        $sql = $this->queryStart . 'WHERE Design.name = :name';
 
         $statement = $this->database->prepare($sql);
         $statement->bindValue(':name', $name, PDO::PARAM_STR);
@@ -83,42 +93,133 @@ SQL;
 
         $array = $this->parseResults($statement->fetchAll(PDO::FETCH_ASSOC));
 
-        return $this->convertArrayToObject($array);
+        return $this->convertArrayToObject(reset($array));
+    }
+
+    public function getAll()
+    {
+        $parsedArray = $this->parseResults($this->database->query($this->queryStart)->fetchAll(PDO::FETCH_ASSOC));
+
+        foreach ($parsedArray as $itemArray) {
+            $items[] = $this->convertArrayToObject($itemArray);
+        }
+
+        return $items;
+    }
+
+    public function getQueue($start = 0, $limit = 100)
+    {
+        $statement = $this->database->prepare($this->queryStart . 'WHERE Design.displayDate >= :FutureDate Limit :Start, :Amount');
+
+        $futureDate = $this->settings->getFeaturedDates()[3];
+
+        $statement->bindValue('FutureDate', $futureDate->format('Y-m-d'));
+        $statement->bindValue('Start', $start, PDO::PARAM_INT);
+        $statement->bindValue('Amount', $limit, PDO::PARAM_INT);
+
+        $statement->execute();
+
+        foreach ($this->parseResults($statement->fetchAll(PDO::FETCH_ASSOC)) as $itemArray) {
+            $items[] = $this->convertArrayToObject($itemArray);
+        }
+
+        return $items;
+    }
+
+    public function getFeatured()
+    {
+        $statement = $this->database->prepare($this->queryStart . 'WHERE Design.displayDate > :PastDate AND Design.displayDate < :FutureDate');
+
+        $dates = $this->settings->getFeaturedDates();
+
+        $pastDate   = $dates[0];
+        $futureDate = $dates[3];
+
+
+        $statement->bindValue('PastDate', $pastDate->format('Y-m-d'));
+        $statement->bindValue('FutureDate', $futureDate->format('Y-m-d'));
+
+        $statement->execute();
+
+        foreach ($this->parseResults($statement->fetchAll(PDO::FETCH_ASSOC)) as $itemArray) {
+            $items[] = $this->convertArrayToObject($itemArray);
+        }
+
+        return $items;
+    }
+
+    public function getStore($start = 0, $limit = 50)
+    {
+        $query     = $this->queryStart . $this->itemCountJoin
+            . 'WHERE Design.displayDate <= :PastDate AND itemsSold.totalSold < Design.salesLimit Limit :Start, :Amount';
+        $statement = $this->database->prepare($query);
+
+        $pastDate = $this->settings->getFeaturedDates()[0];
+
+        $statement->bindValue('PastDate', $pastDate->format('Y-m-d'));
+        $statement->bindValue('Start', $start, PDO::PARAM_INT);
+        $statement->bindValue('Amount', $limit, PDO::PARAM_INT);
+
+        $statement->execute();
+
+        foreach ($this->parseResults($statement->fetchAll(PDO::FETCH_ASSOC)) as $itemArray) {
+            $items[] = $this->convertArrayToObject($itemArray);
+        }
+
+        return $items;
+    }
+
+    public function getVault($start = 0, $limit = 50)
+    {
+        $query     = $this->queryStart . $this->itemCountJoin
+            . 'WHERE itemsSold.totalSold >= Design.salesLimit Limit :Start, :Amount';
+        $statement = $this->database->prepare($query);
+
+        $statement->bindValue('Start', $start, PDO::PARAM_INT);
+        $statement->bindValue('Amount', $limit, PDO::PARAM_INT);
+
+        $statement->execute();
+
+        foreach ($this->parseResults($statement->fetchAll(PDO::FETCH_ASSOC)) as $itemArray) {
+            $items[] = $this->convertArrayToObject($itemArray);
+        }
+
+        return $items;
     }
 
     public function persist($object)
     {
         $this->designFactory->persist($object->getDesign());
-        
-        foreach($object->getArticles() as $article){
+
+        foreach ($object->getArticles() as $article) {
             $this->articleFactory->persist($article);
         }
-        
-        foreach($object->getProducts() as $product){
+
+        foreach ($object->getProducts() as $product) {
             $this->productFactory->persist($product);
         }
-        
+
         unset($object);
     }
 
     /** Stub, don't use */
     public function create(){ }
-    
+
     /** Stub, don't use */
     protected function convertObjectToArray($object){ }
 
     protected function convertArrayToObject($array)
     {
         $design = $this->designFactory->convertArrayToObject($array['design']);
-        
-        foreach($array['articles'] as $articleArray){
+
+        foreach ($array['articles'] as $articleArray) {
             $articles[$articleArray['ID']] = $this->articleFactory->convertArrayToObject($articleArray);
         }
-        
-        foreach($array['products'] as $productArray){
+
+        foreach ($array['products'] as $productArray) {
             $products[$productArray['ID']] = $this->productFactory->convertArrayToObject($productArray);
         }
-        
+
         return parent::convertArrayToObject(['design' => $design, 'articles' => $articles, 'products' => $products]);
     }
 
@@ -127,24 +228,24 @@ SQL;
         $return = [];
 
         foreach ($array as $row) {
-            $return['design']['ID']                                   = $row['designID'];
-            $return['design']['name']                                 = $row['name'];
-            $return['design']['displayDate']                          = $row['displayDate'];
-            $return['design']['designImageURL']                       = $row['designImageURL'];
-            $return['design']['salesLimit']                           = $row['salesLimit'];
-            $return['design']['votes']                                = $row['votes'];
-            $return['articles'][$row['articleID']]['ID']              = $row['articleID'];
-            $return['articles'][$row['articleID']]['designID']        = $row['designID'];
-            $return['articles'][$row['articleID']]['productID']       = $row['productID'];
-            $return['articles'][$row['articleID']]['lastUpdated']     = $row['lastUpdated'];
-            $return['articles'][$row['articleID']]['description']     = $row['description'];
-            $return['articles'][$row['articleID']]['articleImageURL'] = $row['articleImageURL'];
-            $return['articles'][$row['articleID']]['numberSold']      = $row['numberSold'];
-            $return['articles'][$row['articleID']]['baseRetail']      = $row['baseRetail'];
-            $return['products'][$row['productID']]['ID']              = $row['productID'];
-            $return['products'][$row['productID']]['cost']            = $row['cost'];
-            $return['products'][$row['productID']]['type']            = $row['type'];
-            $return['products'][$row['productID']]['sizesAvailable']  = $row['sizesAvailable'];
+            $return[$row['designID']]['design']['ID']                                   = $row['designID'];
+            $return[$row['designID']]['design']['name']                                 = $row['name'];
+            $return[$row['designID']]['design']['displayDate']                          = $row['displayDate'];
+            $return[$row['designID']]['design']['designImageURL']                       = $row['designImageURL'];
+            $return[$row['designID']]['design']['salesLimit']                           = $row['salesLimit'];
+            $return[$row['designID']]['design']['votes']                                = $row['votes'];
+            $return[$row['designID']]['articles'][$row['articleID']]['ID']              = $row['articleID'];
+            $return[$row['designID']]['articles'][$row['articleID']]['designID']        = $row['designID'];
+            $return[$row['designID']]['articles'][$row['articleID']]['productID']       = $row['productID'];
+            $return[$row['designID']]['articles'][$row['articleID']]['lastUpdated']     = $row['lastUpdated'];
+            $return[$row['designID']]['articles'][$row['articleID']]['description']     = $row['description'];
+            $return[$row['designID']]['articles'][$row['articleID']]['articleImageURL'] = $row['articleImageURL'];
+            $return[$row['designID']]['articles'][$row['articleID']]['numberSold']      = $row['numberSold'];
+            $return[$row['designID']]['articles'][$row['articleID']]['baseRetail']      = $row['baseRetail'];
+            $return[$row['designID']]['products'][$row['productID']]['ID']              = $row['productID'];
+            $return[$row['designID']]['products'][$row['productID']]['cost']            = $row['cost'];
+            $return[$row['designID']]['products'][$row['productID']]['type']            = $row['type'];
+            $return[$row['designID']]['products'][$row['productID']]['sizesAvailable']  = $row['sizesAvailable'];
         }
 
         return $return;
