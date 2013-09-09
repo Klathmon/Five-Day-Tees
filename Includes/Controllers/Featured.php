9b5a3c1ac421d5f24dfce3b77d1fd21f5c333553
Greg <gregbenner1@gmail.com>
2013-09-09 19:45:25 -0400

    Updated Featured page to use new objects

--- Includes/Controllers/Featured.php
+++ Includes/Controllers/Featured.php
@@ -9,10 +9,22 @@ $layout = new Layout($config, 'Featured.tpl', 'Featured', 'Page_Featured');
 if (!$layout->isPageCached()) {
     //If the page is cached, then skip all of this because it's not needed or used
     $settings    = new Settings($database, $config);
-    $itemsMapper = new \Mapper\Item($database, $config);
+    $itemsFactory = new \Factory\Item($database, $settings);
 
-    $layout->assign('settings', $settings);
-    $layout->assign('items', $itemsMapper->getFeatured(true));
+    $items = $itemsFactory->getFeatured();
+    
+    foreach($items as $item){
+        $category = $settings->getItemCategory($item);
+        $itemsDisplay[] = [
+            'URLName' => $item->getURLName(),
+            'name' => $item->getDesign()->getName(),
+            'description' => $item->getFirstArticle()->getDescription(),
+            'price' => $settings->getItemCurrentPrice($item->getFirstArticle()->getBaseRetail(), $category)->getNiceFormat(),
+            'designImageURL' => $item->getDesign()->getFormattedDesignImage(150, 150, 'jpg')
+        ];
+    }
+    
+    $layout->assign('items', $itemsDisplay);
 }
 
 $layout->output();
